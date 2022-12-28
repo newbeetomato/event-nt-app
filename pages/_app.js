71ef02dc5fd9485d18d0d6084a7c92ca42b4e949
ibrahimnureddin <ibrahimnureddin4job@gmail.com>
2022-12-28 18:37:28 +0300

    Components

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import MainLayout from '../src/components/layout/main-layout';
-
+import "../styles/globals.css";
+import MainLayout from "../src/components/layout/main-layout";
 
 // maine git orda anlatıldı
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
     </>
-  )
+  );
 }
